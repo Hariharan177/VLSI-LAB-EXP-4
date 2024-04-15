e12bd50a4f352a3b6e7e96fea906fa157889efb3
Hariharan177 <164841000+Hariharan177@users.noreply.github.com>
2024-04-15 10:38:29 +0530

    Update Exp4_SRflipflop.v

--- Exp4_SRflipflop.v
+++ Exp4_SRflipflop.v
@@ -1,13 +1,20 @@
-module sr( s,r,clk,q);
- input s,r,clk; 
-output reg q=0; 
-
-
-
-
+module SR_flipflop (q, q_bar, s,r, clk, reset); 
+  input s,r,clk, reset;
+  output reg q;
+  output q_bar;
+  // always@(posedge clk or negedge reset) // for asynchronous reset
+  always@(posedge clk) begin // for synchronous reset
+    if(!reset)
+     q <= 0;
+    else 
+  begin
+      case({s,r})
+        2'b00: q <= q;    // No change
+        2'b01: q <= 1'b0; // reset
+        2'b10: q <= 1'b1; // set
+        2'b11: q <= 1'bx; // Invalid inputs
+      endcase
+    end
+  end
+  assign q_bar = ~q;
 endmodule
-
-
-
-
-
