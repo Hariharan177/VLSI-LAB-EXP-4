7591a40f56814028b59f2b1febc6915eb96633ef
Hariharan177 <164841000+Hariharan177@users.noreply.github.com>
2024-04-15 10:50:35 +0530

    Update Exp4_updowncounter.v

--- Exp4_updowncounter.v
+++ Exp4_updowncounter.v
@@ -1,9 +1,21 @@
-//4 BIT UP/DOWN COUNTER 
-module updowncounter(clk,rst,ud,out); 
-input clk,rst,ud; 
-output reg[3:0]out = 4’b0000;
-
-
+module upordown_counter(Clk, reset, UpOrDown,  Count);
+input Clk,reset,UpOrDown;
+output [3 : 0] Count;
+reg [3 : 0] Count = 0;
+always @(posedge(Clk) or posedge(reset))
+begin
+  if(reset == 1)
+    Count <= 0;
+  else
+    if(UpOrDown == 1)  
+    if(Count == 15)
+      Count <= 0;
+  else
+    Count <= Count + 1; 
+  else
+    if(Count == 0)
+      Count <= 15;
+  else
+    Count <= Count - 1; 
+end
 endmodule
-
-
